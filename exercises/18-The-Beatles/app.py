dbba6f91478dd19543ca5245b771543a56359914
Daniel Martinez <d.martinez74@gmail.com>
2023-05-13 02:19:25 +0000

    done until 19

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,1 +1,15 @@
 # Your code here!!
+def sing() :
+    let_it_be = ""
+    for i in range (1,13):
+        if (i == 5):
+           let_it_be += "whisper words of wisdom, "
+        elif (i == 11):
+            let_it_be += "there will be an answer, "
+        elif (i==12):
+            let_it_be += "let it be"
+        else:
+            let_it_be += "let it be, "
+    return let_it_be
+
+print(sing())
